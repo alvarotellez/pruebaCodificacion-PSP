7566df88d11696c33352783434294eac0521959b
alvarotellez <alvaro.tellez.hidalgo@gmail.com>
2016-12-22 21:03:40 +0100

    Tercer commit queda terminar el login registro.php y login.php

---
+++ login.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alvarotellez
+ * Date: 22/12/16
+ * Time: 20:53
+ */
--- registro.php
+++ registro.php
@@ -1,15 +1,9 @@
 <?php
-
 /**
  * Created by PhpStorm.
- * User: atellez
+ * User: alvarotellez
  * Date: 22/12/16
- * Time: 13:38
+ * Time: 20:54
  */
 require_once "Connection.php";
 
-class index
-{
-
-
-}
